9beca5732e0036a0c8272488cfb8f7d4ba00efca
zugzwang1919 <russell.wolfe@gmail.com>
2017-01-27 17:03:24 -0500

    Adding .ts files.  Removing generated .js files.

--- web/app/mock-users.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.USERS = [
-    { id: 11, name: 'Russ Wolfe', username: 'russ', password: 'russ', emailAddress: 'russell.wolfe@gmail.com' },
-    { id: 12, name: 'CT Wolfe', username: 'ct', password: 'ct', emailAddress: 'russell.wolfe@gmail.com' },
-];
-//# sourceMappingURL=mock-users.js.map
