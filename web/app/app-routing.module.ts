a1f152ca6f0cba09f25aba252fd12df12144fd8b
zugzwang1919 <russell.wolfe@gmail.com>
2017-01-31 13:59:18 -0500

    Basic stuff now working

---
+++ web/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { UserDetailComponent }  from './user-detail.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/user/1', pathMatch: 'full' },
+  { path: 'user/:id', component: UserDetailComponent },
+  { path: 'user/create', component: UserDetailComponent },
+  { path: 'user', component: UserDetailComponent },
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
