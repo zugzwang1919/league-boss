9f0908a34da6dda9cbc07b4674b87846b7215905
zugzwang1919 <russell.wolfe@gmail.com>
2017-02-04 11:51:29 -0500

    Server changes to allow users to be updated

--- server/logic/user-logic.js
+++ server/logic/user-logic.js
@@ -8,10 +8,7 @@ module.exports = function () {
     createUser: function (userData, callback) {
       console.log("user-logic.create() - createUser has been called.");
       // Ensure all of the required inputs are present
-      if (userData.userName == null ||
-        userData.password == null ||
-        userData.emailAddress == null) {
-
+      if (!isUserValid(userData)) {
         callback(true, "At least one of the required user attributes is missing.");
         return;
       }
@@ -32,9 +29,46 @@ module.exports = function () {
           console.log("user-logic.create() - " + errorMessage);
           if (err.name === 'SequelizeUniqueConstraintError')
             errorMessage = "The requested user name already exists."
-          
+
+          callback(true, errorMessage);
+        })
+    },
+    updateUser: function (userData, callback) {
+      console.log("user-logic.update() - updateUser has been called.");
+
+      // Ensure all of the required inputs are present
+      if (!isUserValid(userData)) {
+        callback(true, "At least one of the required user attributes is missing.");
+        return;
+      }
+
+      // If we're here, we should be able to create a user, so...
+      // Create the user
+      User.update(
+        {
+          userName: userData.userName,
+          password: userData.password,
+          emailAddress: userData.emailAddress,
+        },
+        { where: { id: userData.id } }
+      )
+        .then(result => {
+          console.log("user-logic.update() " + userData.userName + " was successfully updated.");
+          callback(null, null);
+        })
+        .catch(err => {
+          var errorMessage = "A Sequelize error has occurred.  Error Name: " + err.name + ".  Error Message: " + err.message;
+          console.log("user-logic.create() - " + errorMessage);
+          if (err.name === 'SequelizeUniqueConstraintError')
+            errorMessage = "The requested user name already exists."
           callback(true, errorMessage);
         })
     }
   };
+
+  function isUserValid(userData) {
+    return (userData.userName != null &&
+      userData.password != null &&
+      userData.emailAddress != null)
+  }
 }
--- server/rest/user-rest.js
+++ server/rest/user-rest.js
@@ -1,6 +1,6 @@
 
 var express = require('express');
-var router  = express.Router();
+var router = express.Router();
 var userModelUtils = require('../model/user');
 var User = userModelUtils().definition();
 var userLogicUtils = require('../logic/user-logic');
@@ -11,23 +11,23 @@ var restUtils = require('./rest-util.js');
 router.get('/:userId', function (req, res) {
   console.log("Request received on server!  Looking for user with an id of " + req.params.userId);
   User.findById(req.params.userId)
-      .then( user => {
-        if (user != null) {
-          console.log("Inside then!!!")
-          console.log("User id = " + user.id)
-          console.log("User name = " + user.userName)
-          console.log("User password = " + user.password)
-          console.log("User email address = " + user.emailAddress)
-          res.send(user)
-        }
-        else {
-          console.log("user with id " + req.params.userId + " was not found.")
-          res.sendStatus(404)
-        }  
-     });
+    .then(user => {
+      if (user != null) {
+        console.log("Inside then!!!")
+        console.log("User id = " + user.id)
+        console.log("User name = " + user.userName)
+        console.log("User password = " + user.password)
+        console.log("User email address = " + user.emailAddress)
+        res.send(user)
+      }
+      else {
+        console.log("user with id " + req.params.userId + " was not found.")
+        res.sendStatus(404)
+      }
+    });
 });
 
-router.post('/create', function(req, res) {
+router.post('/create', function (req, res) {
   console.log("user-rest createUser: userName found in body = " + req.body.userName);
   console.log("user-rest createUser: password found in body = " + req.body.password);
   console.log("user-rest createUser: emailAddress found in body = " + req.body.emailAddress);
@@ -35,8 +35,8 @@ router.post('/create', function(req, res) {
   userLogicUtils().createUser({
     userName: req.body.userName,
     password: req.body.password,
-    emailAddress: req.body.emailAddress,  
-    },
+    emailAddress: req.body.emailAddress,
+  },
     function (err, data) {
       if (err) {
         console.log("user-rest createUser callback has been entered.  An error occurred.");
@@ -47,9 +47,35 @@ router.post('/create', function(req, res) {
       else {
         console.log("user-rest createUser callback has been entered. Everything was fine.");
         res.statusCode = 200;
-        res.json(restUtils().buildJSONfromMessage("Success!"));  
+        res.json(restUtils().buildJSONfromMessage("Success!"));
       }
-  }) ;
+    });
+});
 
+
+router.put('/:userId', function (req, res) {
+  console.log("user-rest updateUser: userName found in body = " + req.body.userName);
+  console.log("user-rest updateUser: password found in body = " + req.body.password);
+  console.log("user-rest updateUser: emailAddress found in body = " + req.body.emailAddress);
+
+  userLogicUtils().updateUser({
+    id: req.body.id,
+    userName: req.body.userName,
+    password: req.body.password,
+    emailAddress: req.body.emailAddress,
+  },
+    function (err, data) {
+      if (err) {
+        console.log("user-rest updateUser callback has been entered.  An error occurred.");
+        console.log("error message = " + data);
+        res.statusCode = 400;
+        res.json(restUtils().buildJSONfromMessage(data));
+      }
+      else {
+        console.log("user-rest createUser callback has been entered. Everything was fine.");
+        res.statusCode = 200;
+        res.json(restUtils().buildJSONfromMessage("Success!"));
+      }
+    });
 });
 module.exports = router;
