178253883a840fd43392a74409cc40b36c06ca4b
zugzwang1919 <russell.wolfe@gmail.com>
2017-03-22 20:53:22 -0400

    UI for SuperUser

--- web/app/user/mock-users.ts
+++ web/app/user/mock-users.ts
@@ -1,7 +1,7 @@
 import { User } from './user';
 
 export const USERS: User[] = [
-  { id: 11, userName: 'russ', password: 'russ', emailAddress: 'russell.wolfe@gmail.com' },
-  { id: 12, userName: 'ct', password: 'ct', emailAddress: 'russell.wolfe@gmail.com' },
+  { id: 11, userName: 'russ', password: 'russ', emailAddress: 'russell.wolfe@gmail.com', isSuperUser: true},
+  { id: 12, userName: 'ct', password: 'ct', emailAddress: 'russell.wolfe@gmail.com', isSuperUser: false },
 
 ];
