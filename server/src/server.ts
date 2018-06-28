ee8374da955960a6c1f4f707f4b63e23cd14ce2f
Russ <rwolfe@opentext.com>
2018-06-28 16:12:03 -0400

    Cleanup some comments.

--- server/src/server.ts
+++ server/src/server.ts
@@ -37,12 +37,16 @@ export class Server {
     // Indicate that we'll be using body-parser for the JSON in the bodies of requests and responses
     this.app.use(bodyParser.json());
 
-    // I had to add this to allow javascript served from localhost:3000 to talk to localhost:1919
-    // Can't say that I truly understand it
+    // Add some headers to all responses
     this.app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+      // Allow javascript served from any site to see responses from this server
+      // (Once I decide where the javascript will be served from, I could limit this)
       res.header("Access-Control-Allow-Origin", "*");
+      // When responding to any preflight request, indicate that we accept all verbs
       res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+      // When responding to any preflight request, indicate which headers are allowed
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Wolfe-Authentication-Token");
+      // Don't filter out our Wolfe-Authenticaion-Token
       res.header("Access-Control-Expose-Headers", "Wolfe-Authentication-Token");
       next();
     });
